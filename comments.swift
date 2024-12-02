33e10c730866c4754faddd938ae73c2c7cf07e70
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-02 14:19:23 -0800

    Comments in swift

---
+++ comments.swift
@@ -0,0 +1,9 @@
+// This is a comment in swift
+
+/* This is also a comment 
+but is written over multiple lines 
+*/
+
+/* This is the start of the first multiline comment.
+    /* This is the second, nested multiline comment. */
+This is the end of the first multiline comment. */
