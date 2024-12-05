665aa772d3f33714e96bc46dc6a8761c856041d4
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-05 11:07:10 -0800

    Notes on dictorinaries

--- arrays.swift
+++ arrays.swift
@@ -1,5 +1,3 @@
-import UIKit
-
 var evenNumbers = [2, 4, 6, 8]
 var songs: [String] = ["Shake it Off", "You Belong with Me", "Back to December"]
 
---
+++ dictionaries.swift
@@ -0,0 +1,21 @@
+var person = [
+                "first": "Taylor",
+                "middle": "Alison",
+                "last": "Swift",
+                "month": "December",
+                "website": "taylorswift.com"
+            ]
+
+person["middle"]
+person["month"]
+
+var person2: [String : String] = [
+                "first": "Taylor",
+                "middle": "Alison",
+                "last": "Swift",
+                "month": "December",
+                "website": "taylorswift.com"
+            ]
+
+person2["middle"]
+person2["month"]
