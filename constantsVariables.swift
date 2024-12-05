592f5471e03847d8ced8a51af51acf5c4206512c
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-12-04 17:25:51 -0800

    More notes

--- constantsVariables.swift
+++ constantsVariables.swift
@@ -65,3 +65,21 @@ var missABeat: Bool
 missABeat = false
 
 var name2: String = "Tim McGraw"
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a * a
+
+var b = 10
+b += 10
+b -= 10
+
+var c = 1.1
+var d = 2.2
+var e = c + d
+
+var name1 = "Tim McGraw"
+var name2 = "Romeo"
+var both = name1 + " and " + name2
+both
